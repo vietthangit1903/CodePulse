4c1f44b42783992c3782e6a32d04ae72c3cccd93
VIETTHANG <vietphamthang2001@gmail.com>
2024-05-12 17:07:19 +0700

    List all category function

--- CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -16,10 +16,10 @@ namespace CodePulse.API.Controllers
         public CategoriesController(ICategoryRepository categoryRepository)
         {
             _categoryRepository = categoryRepository;
-        }   
+        }
 
         [HttpPost]
-        public async Task<IActionResult> CreateCategoryAsync([FromBody]CreateCategoryRequestDTO createCategoryRequest)
+        public async Task<IActionResult> CreateCategoryAsync([FromBody] CreateCategoryRequestDTO createCategoryRequest)
         {
             var newCategory = new Category
             {
@@ -37,7 +37,26 @@ namespace CodePulse.API.Controllers
             };
 
             return Ok(response);
-            
+
+        }
+
+        //GET: https://localhost:7070/api/Categories
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategoriesAsync()
+        {
+            var categories = await _categoryRepository.GetAllCategoriesAsync();
+            var response = new List<CategoryDTO>();
+            //Map Domain model to DTO
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDTO()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+            }
+            return Ok(response);
         }
     }
 }
--- CodePulse.UI/codepulse/src/app/features/category/services/category.service.ts
+++ CodePulse.UI/codepulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +13,14 @@ export class CategoryService {
 
   addCategory(category: AddCategoryRequest): Observable<void> {
     return this.http.post<void>(
-      `https://localhost:7070/api/Categories`,
+      `${environment.apiBaseUrl}/api/Categories`,
       category
+    );
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/Categories`
     );
   }
 }
