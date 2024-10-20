fb5449af33a6ce80af55e391d70e9c36159d6ad8
VIETTHANG <vietphamthang2001@gmail.com>
2024-10-20 17:42:27 +0700

    10: Update entity relationship

--- CodePulse.UI/codepulse/src/app/features/blog-post/add-blog-post/add-blog-post.component.ts
+++ CodePulse.UI/codepulse/src/app/features/blog-post/add-blog-post/add-blog-post.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blog-post',
   templateUrl: './add-blog-post.component.html',
   styleUrls: ['./add-blog-post.component.css'],
 })
-export class AddBlogPostComponent {
+export class AddBlogPostComponent implements OnInit {
   model: AddBlogPost;
+  categories$?: Observable<Category[]>
   constructor(
     private _blogPostService: BlogPostService,
+    private _categoriesService: CategoryService,
     private _router: Router
   ) {
     this.model = {
@@ -23,8 +28,14 @@ export class AddBlogPostComponent {
       author: '',
       publishedDate: new Date(),
       isVisible: true,
+      categories: [],
     };
   }
+
+  ngOnInit(): void {
+    this.categories$ = this._categoriesService.getAllCategories();
+  }
+
   onFormSubmit(): void {
     this._blogPostService.createBlogPost(this.model).subscribe({
       next: (response) => {
